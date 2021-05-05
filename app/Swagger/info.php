15b7c3d0058a339e2126ae1a03bcde6ee3d848a6
FRL-GQ128S2\Dominique <dominique.bureau@decathlon.com>
2021-05-05 17:14:26 +0200

    swagger

---
+++ app/Swagger/info.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="ClassifiedAds - Api Documentation,oh yeah  !!!",
+ *      description="Here you will find all the necessary documentation to use the endpoints."
+ * )
+ *
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Demo API Server"
+ * )
+ *
+ * @OAS\SecurityScheme(
+ *      securityScheme="bearer_token",
+ *      type="http",
+ *      scheme="bearer"
+ * )
+ *
+ * @OA\Tag(
+ *     name="Organizations",
+ *     description="API Endpoints of Organizations"
+ * )
+ *
+ * @OA\Tag(
+ *     name="Artists",
+ *     description="API Endpoints of Artists"
+ * )
+ */
