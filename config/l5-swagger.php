9dc7547e209ad7b462c98028e7c29fe870405c39
FRL-GQ128S2\Dominique <dominique.bureau@decathlon.com>
2021-05-07 11:33:14 +0200

    swagger with https

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'Music Domi',
+                'title' => 'Classified-ads my company',
             ],
             'routes' => [
                 /*
